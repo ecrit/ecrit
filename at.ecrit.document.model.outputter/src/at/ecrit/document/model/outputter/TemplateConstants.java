6ee625dd6a3f69cdd11acde2c1385bf1b3f08f88
Marco Descher <marco@descher.at>
2014-04-01 14:32:48 +0200

    Removed non-required bundle at.ecrit.document.model.outputter

--- at.ecrit.document.model.outputter/src/at/ecrit/document/model/outputter/TemplateConstants.java
+++
@@ -1,6 +0,0 @@
-package at.ecrit.document.model.outputter;
-
-public class TemplateConstants {
-	public static final String HTML_TEMPLATE = "htmlTemplate.ftl";
-	public static final String LATEX_TEMPLATE = "latexTemplate.ftl";
-}
