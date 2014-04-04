9164681f12c7aa93723947757288c099d7a7c5ce
lamann <amann@medevit.at>
2014-04-04 12:40:23 +0200

    editor adaptions/bug fix

--- at.ecrit.e4.tools.extension.core/src/at/ecrit/e4/tools/extension/core/control/MapEntryEditorComposite.java
+++ at.ecrit.e4.tools.extension.core/src/at/ecrit/e4/tools/extension/core/control/MapEntryEditorComposite.java
@@ -1,26 +1,16 @@
 package at.ecrit.e4.tools.extension.core.control;
 
-import java.util.Iterator;
-
 import org.eclipse.core.databinding.observable.value.WritableValue;
 import org.eclipse.emf.databinding.EMFDataBindingContext;
 import org.eclipse.emf.edit.domain.EditingDomain;
-import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.TextViewer;
 import org.eclipse.jface.text.contentassist.ContentAssistant;
+import org.eclipse.jface.text.contentassist.IContentAssistant;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.text.source.SourceViewer;
-import org.eclipse.mylyn.internal.wikitext.ui.WikiTextUiPlugin;
-import org.eclipse.mylyn.internal.wikitext.ui.editor.MarkupEditor;
-import org.eclipse.mylyn.wikitext.core.parser.markup.MarkupLanguage;
-import org.eclipse.mylyn.wikitext.textile.core.TextileLanguage;
-import org.eclipse.mylyn.wikitext.ui.editor.MarkupSourceViewerConfiguration;
+import org.eclipse.jface.text.source.SourceViewerConfiguration;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.StyledText;
-import org.eclipse.swt.events.DisposeEvent;
-import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.events.KeyAdapter;
 import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.SelectionAdapter;
@@ -30,14 +20,6 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.editors.text.EditorsUI;
-import org.eclipse.ui.swt.IFocusService;
-import org.eclipse.ui.texteditor.AbstractDecoratedTextEditorPreferenceConstants;
-import org.eclipse.ui.texteditor.AnnotationPreference;
-import org.eclipse.ui.texteditor.DefaultMarkerAnnotationAccess;
-import org.eclipse.ui.texteditor.MarkerAnnotationPreferences;
-import org.eclipse.ui.texteditor.SourceViewerDecorationSupport;
 
 import at.ecrit.e4.tools.extension.core.assistance.TextileContentAssistProcessor;
 import at.ecrit.e4.tools.extension.core.assistance.TextileTagTracker;
@@ -46,7 +28,7 @@ import at.ecrit.e4.tools.extension.core.listener.PersistedStateValueChangeListen
 public class MapEntryEditorComposite extends AbstractEditorComposite {
 	
 	final WritableValue value_description = new WritableValue("", String.class);
-	Text checked;
+	private Text checked;
 	
 	/**
 	 * Create the composite.
@@ -78,37 +60,25 @@ public class MapEntryEditorComposite extends AbstractEditorComposite {
 			gd_l.widthHint = 100;
 			l.setLayoutData(gd_l);
 			
-			// final Text text = new Text(this, SWT.BORDER | SWT.WRAP | SWT.MULTI);
-			// GridData gd_text = new GridData(GridData.FILL_BOTH);
-			// gd_text.horizontalSpan = 2;
-			// text.setLayoutData(gd_text);
-			//
-			// context.bindValue(textProp.observeDelayed(200, text),
-			// value_description);
-			
-			TextViewer textViewer =
-				new TextViewer(this, SWT.MULTI | SWT.BORDER | SWT.WRAP | SWT.V_SCROLL);
+			final SourceViewer viewer =
+				new SourceViewer(this, null, SWT.MULTI | SWT.BORDER | SWT.WRAP | SWT.V_SCROLL);
 			GridData gd_Viewer = new GridData(GridData.FILL_BOTH);
 			gd_Viewer.horizontalSpan = 2;
-			textViewer.getTextWidget().setLayoutData(gd_Viewer);
-			textViewer.setDocument(new Document());
-			
-			final ContentAssistant assistant = new ContentAssistant();
-			assistant.setContentAssistProcessor(new TextileContentAssistProcessor(
-				new TextileTagTracker()), IDocument.DEFAULT_CONTENT_TYPE);
-			// assistant.setProposalPopupOrientation(0);
-			assistant.install(textViewer);
-			
-			textViewer.getControl().addKeyListener(new KeyAdapter() {
+			viewer.getControl().setLayoutData(gd_Viewer);
+			viewer.setDocument(new Document());
+			viewer.setEditable(true);
+			viewer.configure(new TextileSourceViewerConfiguration());
+			viewer.getControl().addKeyListener(new KeyAdapter() {
 				public void keyPressed(KeyEvent e){
-					if ((e.character == ' ') && ((e.stateMask & SWT.CTRL) != 0)) {
-						assistant.showPossibleCompletions();
+					if ((e.character == SWT.SPACE) && ((e.stateMask & SWT.CTRL) != 0)) {
+						viewer.doOperation(ISourceViewer.CONTENTASSIST_PROPOSALS);
 					} else {
 						// ignore
 					}
 				}
 			});
-			context.bindValue(textProp.observeDelayed(200, textViewer.getTextWidget()),
+			
+			context.bindValue(textProp.observeDelayed(200, viewer.getTextWidget()),
 				value_description);
 		}
 	}
@@ -129,12 +99,6 @@ public class MapEntryEditorComposite extends AbstractEditorComposite {
 			gd_l.minimumWidth = 100;
 			gd_l.widthHint = 100;
 			l.setLayoutData(gd_l);
-			new Label(this, SWT.NONE);
-			
-			checked = new Text(this, SWT.NONE);
-			checked.setVisible(false);
-			checked.setText("");
-			new Label(this, SWT.NONE);
 			
 			Button btnCheck = new Button(this, SWT.CHECK);
 			btnCheck.setSelection(defaultValue);
@@ -148,108 +112,30 @@ public class MapEntryEditorComposite extends AbstractEditorComposite {
 				};
 			});
 			
+			checked = new Text(this, SWT.NONE);
+			checked.setVisible(false);
+			checked.setText("");
+			
 			context.bindValue(textProp.observeDelayed(200, checked), value_description);
 		}
 	}
 	
-	/**
-	 * would work as well but has some bugs - FIXME
-	 * 
-	 * @param parent
-	 * @param style
-	 * @return
-	 */
-	private SourceViewer createTextileEditor(Composite parent, int style){
-		SourceViewer viewer = new SourceViewer(parent, null, style | SWT.WRAP);
-		viewer.setDocument(new Document());
-		viewer.getControl().setLayoutData(new GridData(GridData.FILL_BOTH));
-		// configure the viewer
-		MarkupSourceViewerConfiguration configuration =
-			new MarkupSourceViewerConfiguration(WikiTextUiPlugin.getDefault().getPreferenceStore());
-		configuration.setMarkupLanguage(new TextileLanguage());
-		viewer.configure(configuration);
-		viewer.getTextWidget().setData(MarkupLanguage.class.getName(), new TextileLanguage());
-		viewer.getTextWidget().setData(ISourceViewer.class.getName(), viewer);
+	class TextileSourceViewerConfiguration extends SourceViewerConfiguration {
 		
-		// we want the viewer to show annotations
-		viewer.showAnnotations(true);
-		viewer.showAnnotationsOverview(true);
-		viewer.setEditable(true);
-		
-		DefaultMarkerAnnotationAccess annotationAccess = new DefaultMarkerAnnotationAccess();
-		MarkerAnnotationPreferences annotationPreferences = new MarkerAnnotationPreferences();
-		
-		// configure viewer annotation/decoration support
-		final SourceViewerDecorationSupport support =
-			new SourceViewerDecorationSupport(viewer, null, annotationAccess,
-				EditorsUI.getSharedTextColors());
-		
-		// hook the support up to the preference store
-		Iterator<AnnotationPreference> e =
-			annotationPreferences.getAnnotationPreferences().iterator();
-		while (e.hasNext()) {
-			AnnotationPreference preference = e.next();
-			support.setAnnotationPreference(preference);
+		public TextileSourceViewerConfiguration(){
+			super();
 		}
 		
-		support.setCursorLinePainterPreferenceKeys(
-			AbstractDecoratedTextEditorPreferenceConstants.EDITOR_CURRENT_LINE,
-			AbstractDecoratedTextEditorPreferenceConstants.EDITOR_CURRENT_LINE_COLOR);
-		support.setMarginPainterPreferenceKeys(
-			AbstractDecoratedTextEditorPreferenceConstants.EDITOR_PRINT_MARGIN,
-			AbstractDecoratedTextEditorPreferenceConstants.EDITOR_PRINT_MARGIN_COLOR,
-			AbstractDecoratedTextEditorPreferenceConstants.EDITOR_PRINT_MARGIN_COLUMN);
-		
-		StyledText styledText = viewer.getTextWidget();
-		GridDataFactory.fillDefaults().hint(400, SWT.DEFAULT).grab(true, true).applyTo(styledText);
-		
-		support.install(EditorsUI.getPreferenceStore());
-		styledText.addDisposeListener(new DisposeListener() {
+		@Override
+		public IContentAssistant getContentAssistant(ISourceViewer sourceViewer){
+			ContentAssistant assistant = new ContentAssistant();
+			assistant.setContentAssistProcessor(new TextileContentAssistProcessor(
+				new TextileTagTracker()), IDocument.DEFAULT_CONTENT_TYPE);
+			assistant.setProposalPopupOrientation(IContentAssistant.PROPOSAL_OVERLAY);
+			assistant.setContextInformationPopupOrientation(IContentAssistant.CONTEXT_INFO_ABOVE);
+			assistant.install(sourceViewer);
 			
-			@Override
-			public void widgetDisposed(DisposeEvent e){
-				support.uninstall();
-			}
-		});
-		
-		final ContentAssistant assistant = new ContentAssistant();
-		assistant.setContentAssistProcessor(new TextileContentAssistProcessor(
-			new TextileTagTracker()), IDocument.DEFAULT_CONTENT_TYPE);
-		assistant.install(viewer);
-		
-		viewer.getTextWidget().addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyReleased(KeyEvent e){
-				if (isRelevantKeyCode(e.keyCode)) {
-					assistant.showPossibleCompletions();
-				}
-			}
-			
-			private boolean isRelevantKeyCode(int keyCode){
-				// for some reason not all key presses result in a selection change
-				switch (keyCode) {
-				case SWT.ARROW_DOWN:
-				case SWT.ARROW_LEFT:
-				case SWT.ARROW_RIGHT:
-				case SWT.ARROW_UP:
-				case SWT.PAGE_DOWN:
-				case SWT.PAGE_UP:
-					return true;
-				}
-				return false;
-			}
-		});
-		
-		IFocusService focusService =
-			(IFocusService) PlatformUI.getWorkbench().getService(IFocusService.class);
-		if (focusService != null) {
-			focusService.addFocusTracker(viewer.getTextWidget(), MarkupEditor.EDITOR_SOURCE_VIEWER);
+			return assistant;
 		}
-		return viewer;
-	}
-	
-	@Override
-	protected void checkSubclass(){
-		// Disable the check that prevents subclassing of SWT components
 	}
 }
