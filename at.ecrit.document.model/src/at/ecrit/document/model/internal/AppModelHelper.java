edf54fa8da7029ad2097633b215f22cac6ce3755
Marco Descher <marco@descher.at>
2014-04-28 12:36:31 +0200

    Be more null tolerant

--- at.ecrit.document.model/src/at/ecrit/document/model/internal/AppModelHelper.java
+++ at.ecrit.document.model/src/at/ecrit/document/model/internal/AppModelHelper.java
@@ -10,85 +10,96 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 
 public class AppModelHelper {
-	
+
 	private static MApplication application;
 	private static EModelService modelService;
 	private static Resource appModelResource;
-	
-	public static void initAppModel(EModelService modelService, Resource appModelResource){
-		AppModelHelper.application = (MApplication) appModelResource.getContents().get(0);
+
+	public static void initAppModel(EModelService modelService,
+			Resource appModelResource) {
+		AppModelHelper.application = (MApplication) appModelResource
+				.getContents().get(0);
 		AppModelHelper.appModelResource = appModelResource;
 		AppModelHelper.modelService = modelService;
 	}
-	
-	public static ElementDocumentation getElementDocumentation(String elementId){
+
+	public static ElementDocumentation getElementDocumentation(String elementId) {
 		if (elementId == null || elementId.length() == 0)
 			throw new IllegalArgumentException();
-		
+
 		// modelService only finds MUIElements not, all MApplicationElements
 		MApplicationElement mae = findMUIElement(elementId);
 		if (mae == null) {
 			System.out.println("Could not find element " + elementId);
 			return new ElementDocumentation("", "", "");
 		}
-		
+
 		Map<String, String> persistedState = mae.getPersistedState();
-		return new ElementDocumentation(persistedState.get(Constants.PERSISTENT_STATE_DESCRIPTION),
-			persistedState.get(Constants.PERSISTENT_STATE_PRECONDITION),
-			persistedState.get(Constants.PERSISTENT_STATE_POSTCONDITION));
+		return new ElementDocumentation(
+				persistedState.get(Constants.PERSISTENT_STATE_DESCRIPTION),
+				persistedState.get(Constants.PERSISTENT_STATE_PRECONDITION),
+				persistedState.get(Constants.PERSISTENT_STATE_POSTCONDITION));
 	}
-	
-	public static ApplicationDocumentation getApplicationDocumentation(){
+
+	public static ApplicationDocumentation getApplicationDocumentation() {
 		if (application == null)
 			throw new IllegalArgumentException();
-		
+
 		Map<String, String> persistedState = application.getPersistedState();
-		return new ApplicationDocumentation(persistedState.get(Constants.PERSISTENT_STATE_ABOUT),
-			persistedState.get(Constants.PERSISTENT_STATE_INSTALL),
-			persistedState.get(Constants.PERSISTENT_STATE_MULTIUSER),
-			persistedState.get(Constants.PERSISTENT_STATE_REQUIRES_LOGIN),
-			persistedState.get(Constants.PERSISTENT_STATE_PERSPECTIVE_SWITCH));
+		return new ApplicationDocumentation(
+				persistedState.get(Constants.PERSISTENT_STATE_ABOUT),
+				persistedState.get(Constants.PERSISTENT_STATE_INSTALL),
+				persistedState.get(Constants.PERSISTENT_STATE_MULTIUSER),
+				persistedState.get(Constants.PERSISTENT_STATE_REQUIRES_LOGIN),
+				persistedState
+						.get(Constants.PERSISTENT_STATE_PERSPECTIVE_SWITCH));
 	}
-	
-	public static PerspectiveStackDocumentation getPerspectiveStackDocumentation(String elementId){
+
+	public static PerspectiveStackDocumentation getPerspectiveStackDocumentation(
+			String elementId) {
 		if (elementId == null || elementId.length() == 0) {
 			throw new IllegalArgumentException();
 		}
-		
+
 		// modelService only finds MUIElements not, all MApplicationElements
 		MApplicationElement mae = findMUIElement(elementId);
 		if (mae == null) {
 			System.out.println("Could not find element " + elementId);
 			return new PerspectiveStackDocumentation("");
 		}
-		
+
 		Map<String, String> persistedState = mae.getPersistedState();
 		return new PerspectiveStackDocumentation(
-			persistedState.get(Constants.PERSISTENT_STATE_PERSPECTIVE_SWITCH));
+				persistedState
+						.get(Constants.PERSISTENT_STATE_PERSPECTIVE_SWITCH));
 	}
-	
-	private static MApplicationElement findMUIElement(String elementId){
+
+	private static MApplicationElement findMUIElement(String elementId) {
 		// modelService only finds MUIElements not, all MApplicationElements
 		MApplicationElement mae = modelService.find(elementId, application);
-		
+
 		if (mae == null) {
 			System.out.println("Reverting to iterate search for " + elementId);
-			
-			for (TreeIterator<EObject> i = appModelResource.getAllContents(); i.hasNext();) {
+
+			for (TreeIterator<EObject> i = appModelResource.getAllContents(); i
+					.hasNext();) {
 				EObject object = i.next();
 				if (object instanceof MApplicationElement) {
 					MApplicationElement maeTemp = (MApplicationElement) object;
-					if (maeTemp.getElementId().equalsIgnoreCase(elementId)) {
+					if ((maeTemp != null)
+							&& (maeTemp.getElementId() != null)
+							&& maeTemp.getElementId().equalsIgnoreCase(
+									elementId)) {
 						mae = maeTemp;
 					}
 				}
 			}
-// if (mae == null) {
-// System.out.println("Could not find element " + elementId);
-// return new ElementDocumentation("", "", "");
-// }
+			// if (mae == null) {
+			// System.out.println("Could not find element " + elementId);
+			// return new ElementDocumentation("", "", "");
+			// }
 		}
 		return mae;
 	}
-	
+
 }
